12ee64015d51dff1979b91e76c624c2fe65b325f
infobip-ci <infobip-ci@infobip.com>
2024-04-15 13:50:18 +0200

    iOS SDK Release 2.3.9

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "InfobipRTC",
-            url: "https://rtc.cdn.infobip.com/ios/2/2.3.8/InfobipRTC.zip",
-            checksum: "47800608dc3a12f0295b72b0fb9db9d79c466b9a8951c55fb1531d1193b0a788"
+            url: "https://rtc.cdn.infobip.com/ios/2/2.3.9/InfobipRTC.zip",
+            checksum: "3a5865d70f742c05dfee2e671d75445d79fe472c3b8fe20a22d1b9d984cadf34"
         ),
         .binaryTarget(
             name: "WebRTC",
